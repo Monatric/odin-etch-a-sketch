ab98fd645e272882ce221045aa82f5786068a727
Monatric <nawpnawp8@gmail.com>
2024-02-06 23:22:02 +0800

    Dynamically create a grid based on the number of squares given

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 const divSquare = document.createElement('div');
-const gridContainer = document.querySelector('.grid-container'); 
+const divSquareCont = document.createElement('div');
+const gridContainer = document.querySelector('.grid-container');
+
+divSquare.classList.add('div-square');
+divSquareCont.classList.add('div-square-cont');
+
+function containerMaker(pixels) {
+    let i = 0;
+    while (i < pixels) {
+        const divSquareContClone = divSquareCont.cloneNode(true);
+        gridContainer.appendChild(divSquareContClone);
+        i++;
+    }
+
+    const allDivSquareContClone = document.querySelectorAll('.div-square-cont');    
+    allDivSquareContClone.forEach((element) => {
+        for (let j = 0; j < pixels; j++) {
+            const divSquareClone = divSquare.cloneNode(true);
+            element.appendChild(divSquareClone);
+        }
+    })
+} 
+
+containerMaker(16);
